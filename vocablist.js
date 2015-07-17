5644d39d5834b0378f49178ccd15d6525c4bce05
Stephen Roberts <stephen.roberts4@gmail.com>
2015-07-17 14:30:18 +0200

    Binding data using knockout, fixes some delete bugs

--- vocablist.js
+++ vocablist.js
@@ -1,30 +1,58 @@
+ko.bindingProvider.instance = new ko.secureBindingsProvider();
+
+var VocabModel = function(vocab) {
+    var self = this;
+    self.vocab = ko.observableArray(vocab);
+
+    self.addVocab = function() {
+        self.vocab.push({
+            english: "",
+            spanish: ""
+        });
+    };
+
+    self.removeVocab = function(vocab) {
+        self.vocab.remove(vocab);
+        chrome.storage.local.set({ "vocab": self.vocab() }, function() {
+            console.log("Deleted");
+        });
+    };
+};
+
 chrome.storage.local.get({vocab: []}, function (result) {
 
-    var rows = [];
-    $.each(result.vocab, function(i, item) {
+console.log(result);
+    var viewModel = new VocabModel(result.vocab)
+    ko.applyBindings(viewModel);
 
-        $('#vocab_table').append( '<tr id="row' + i + '"><td>' + item.english + '</td><td>' + item.spanish + '</td></tr>' );
-        var deleteBtn = document.createElement('button')
-        deleteBtn.className = "btn btn-default"
+    // var rows = [];
+    // $.each(result.vocab, function(i, item) {
 
-        var trashIcon = document.createElement('span')
-        trashIcon.className = "glyphicon glyphicon-trash"
+    //     $('#vocab_table').append( '<tr id="row' + i + '"><td>' + item.english + '</td><td>' + item.spanish + '</td></tr>' );
+    //     var deleteBtn = document.createElement('button')
+    //     deleteBtn.className = "btn btn-default"
 
-        deleteBtn.appendChild(trashIcon)
-        deleteBtn.onclick = function() {
+    //     var trashIcon = document.createElement('span')
+    //     trashIcon.className = "glyphicon glyphicon-trash"
 
-            result.vocab.splice(i, 1)
-            chrome.storage.local.set({"vocab": result.vocab}, function() {
-                $('#row' + i).remove()
-            });
-        }
+    //     deleteBtn.appendChild(trashIcon)
+    //     deleteBtn.onclick = function() {
 
-        var tableEl = document.createElement('td')
-        tableEl.style.textAlign = "center"
-        tableEl.appendChild(deleteBtn)
+    //         setButtonProperties("disabled", true)
 
-        $('#row' + i).append(tableEl)
-     });
+    //         result.vocab.splice(i, 1)
+    //         chrome.storage.local.set({"vocab": result.vocab}, function() {
+    //             $('#row' + i).remove()
+    //             setButtonProperties("disabled", false)
+    //         });
+    //     }
+
+    //     var tableEl = document.createElement('td')
+    //     tableEl.style.textAlign = "center"
+    //     tableEl.appendChild(deleteBtn)
+
+    //     $('#row' + i).append(tableEl)
+    //  });
 
      //$('#vocab_table tr:last').after(rows.join(""));
 });
